64c1b33b9afb8281d5c2dacbaa08236212da27ef
JiYeongBae-tech <treevell9652@gmail.com>
2023-12-30 18:26:29 +0900

    선물하기 기능- 뮤텍스 사용

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,14 +1,29 @@
-import { Controller, Get } from '@nestjs/common';
+import {Body, Controller, Get, Logger, Param, Post, Query} from '@nestjs/common';
 import { AppService } from './app.service';
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
+  private readonly logger = new Logger(AppController.name);
 
   //provider로 제공된 app.service 의 엔드포인트 함수를 사용한다는 의미
   @Get('/users')
   getHello(): string {
     return this.appService.getHello();
   }
+
+  @Post('givegift')
+  async giveGift(
+      @Body() data: string
+  ) {
+
+    this.logger.log(`========닉네임 ${data['nickname']} 님이 선물 버튼을 누름`)
+    // 여기에서 뮤텍스를 사용하여 작업을 수행
+    await this.appService.processGift();
+    this.logger.log('@@@@@@@@@@ 뮤텍스 끝 @@@@@@@@@@@2 ')
+    this.logger.log('')
+    return 'Gift given!';
+  }
+
 
 }
--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,29 @@
-import { Injectable } from '@nestjs/common';
+import {Injectable, Logger} from '@nestjs/common';
 
 @Injectable()
 export class AppService {
   getHello(): string {
     return 'Hello World!';
+  }
+
+  private mutex = false;
+  private readonly logger = new Logger(AppService.name);
+
+  async processGift() {
+
+    // uuid 모듈을 가져옵니다.
+    const { v4: uuidv4 } = require('uuid');
+
+    const msguuid = uuidv4();
+    if (!this.mutex) {
+      this.mutex = true;
+      // 여기에서 실제 선물 처리 로직을 수행
+      this.logger.log(`스레드 차지성공. 5초동안 작업시작!... ${msguuid}`);
+      await new Promise((resolve) => setTimeout(resolve, 5000)); // 예시로 5초 동안 처리 중이라 가정
+      this.mutex = false;
+    } else {
+      this.logger.log(`기다리기. 누군가 이미 스레드 차지함... ${msguuid}`);
+      // 다른 사용자가 선물 처리 중일 때의 처리 로직 추가
+    }
   }
 }
